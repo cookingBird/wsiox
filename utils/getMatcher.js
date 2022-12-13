210c04bf5370063dc0ec022e9ef02257e8c7a11d
test <d17381844023@gmail.com>
2022-12-13 22:54:41 +0800

    fix:error handler

--- utils/getMatcher.js
+++ utils/getMatcher.js
@@ -1,20 +1,20 @@
 /**
  * 生成满足ws消息的函数
  * @param {object|function} options 消息匹配条件；满足条件的消息将会被回调函数接收；
- * @param {string} defaultKey 要匹配消息的字段； 
+ * @param {string} defaultKey 要匹配消息的字段；
  * @param {boolean} isReg 是否以正则的方式匹配；
  * @param { function } callback 匹配成功的回调函数；
  * @param { function } errorCallback 匹配成功的回调函数；
- * @returns 
+ * @returns
  */
-export default function (options,defaultKey,callback,errorCallback) {
+export default function (options, defaultKey, callback, errorCallback) {
   const _toString = Object.prototype.toString;
   const isRegExp = (condition) => _toString.call(condition) === '[object RegExp]';
   const isFunc = (condition) => _toString.call(condition) === '[object Function]';
   const isString = (condition) => _toString.call(condition) === '[object String]';
   const isObject = (condition) => _toString.call(condition) === '[object Object]';
   if (!defaultKey || !options) {
-    throw Error('create matcher function failure')
+    throw Error('create matcher function failure');
   }
 
   if (isFunc(options)) {
@@ -22,73 +22,59 @@ export default function (options,defaultKey,callback,errorCallback) {
       try {
         const result = await res;
         if (options(result)) {
-          callback(result)
+          callback(result);
         }
       } catch (e) {
-        const result = e;
-        if (options(result)) {
-          errorCallback ? errorCallback(result)
-            : callback?.(result)
-        }
+        console.error(e);
+        errorCallback ? errorCallback(e) : callback?.(e);
       }
-    }
+    };
   }
   if (!isObject(options)) {
-    throw Error('wsiox on method param[0] error required Func|Object')
+    throw Error('wsiox on method param[0] error required Func|Object');
   }
   const key = options.key ? options.key : defaultKey;
   const condition = options.condition || options[key];
   if (!isRegExp(condition) && !isString(condition) && !isFunc(condition)) {
-    throw Error("wsiox's method on param[0] error, field condition required Reg|String")
+    throw Error("wsiox's method on param[0] error, field condition required Reg|String");
   }
   if (isFunc(condition)) {
     return async (res) => {
       try {
         const result = await res;
         if (condition(result)) {
-          callback(result)
+          callback(result);
         }
       } catch (e) {
-        const result = e;
-        if (condition(result)) {
-          errorCallback ? errorCallback(result)
-            : callback?.(result)
-        }
+        console.error(e);
+        errorCallback ? errorCallback(e) : callback?.(e);
       }
-    }
+    };
   } else if (isRegExp(condition)) {
     return async (res) => {
       try {
         const result = await res;
         const _key = result[key];
         if (condition.test(_key)) {
-          callback(result)
+          callback(result);
         }
       } catch (e) {
-        const result = e;
-        const _key = result[key];
-        if (condition.test(_key)) {
-          errorCallback ? errorCallback(result)
-            : callback?.(result)
-        }
+        console.error(e);
+        errorCallback ? errorCallback(e) : callback?.(e);
       }
-    }
+    };
   } else {
     return async (res) => {
       try {
         const result = await res;
         const _key = result[key];
         if (_key === condition) {
-          callback(result)
+          callback(result);
         }
       } catch (e) {
-        const result = e;
-        const _key = result[key];
-        if (_key === condition) {
-          errorCallback ? errorCallback(result)
-            : callback?.(result)
-        }
+        console.error(e);
+        errorCallback ? errorCallback(e) : callback?.(e);
       }
-    }
+    };
   }
 }
